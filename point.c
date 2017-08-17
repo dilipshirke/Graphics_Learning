a239673597665836fefed3e0493df42433f68c3f
Dilip Shirke <dilipshirke.pune@gmail.com>
2017-08-17 19:10:06 +0530

    render a points
    render a point using GL_POINTS primitive

---
+++ point.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const char *vertexShaderSource = 
+	"#version 330 core\n"
+	"layout (location = 0) in vec2 aPos;"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);\n"
+		
+	"}\n\0";
+
+// Fragment Shader
+const char *fragmentShaderSource = 
+	"#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+	"}\n\0";
+
+
+int main(int argc, char **argv)
+{
+	if ( !glfwInit())
+	{
+		printf("Error: Failed to initialize GLFW\n");
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Points", NULL, NULL);
+	if (window == NULL) 
+	{
+		printf("Failed to create GLFW window\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Error: Failed to initialize GLEW\n");
+		return -1;
+	}
+
+	//point vertices
+	float points[] = 
+	{
+		-0.5f,  0.5f,
+		 0.5f,  0.5f,
+		 0.5f, -0.5f, 
+		-0.5f, -0.5f
+	};
+
+
+	// Shader related stuff
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+		printf("Error: Failed to compile vertex shader.\n");
+	}
+
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if(!success)
+        {
+                printf("Error: Failed to compile fragment shader.\n");
+        }
+
+	//link shaders	
+
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if(!success)
+        {
+                printf("Error: Failed to link shaders.\n");
+        }
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//VBO related activity
+	//declare VAO, VBO 
+	GLuint VAO, VBO, EBO;
+
+	//get unique name/ID
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	//glGenBuffers(1, &EBO);
+	
+	// Bind VAO first, then bind and set VBOs and then configure vertex attributes
+	//bind VAO
+	glBindVertexArray(VAO);
+
+	//bind VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//copy data to GPU
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_POINTS, 0, 4);
+
+	glfwSwapBuffers(window);
+
+	do{
+		glfwPollEvents();
+	}while(glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+return 0;
+
+	
+}
